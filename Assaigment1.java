66d2af72b8770ed2974eb42ff3f5c6dc0fd682c9
Ganapa260694 <144709464+Ganapa260694@users.noreply.github.com>
2023-09-11 21:33:26 +0530

    Assaigment 1
     integer data type programm

---
+++ Assaigment1.java
@@ -0,0 +1,13 @@
+package day2.week1;
+
+public class Assaigment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=5;
+int y=10;
+int z=x*y;
+System.out.println(+z);
+	}
+
+}
