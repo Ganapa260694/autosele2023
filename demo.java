ac33eb51001637eace3c993a36b0797322ae4ce0
Ganapa260694 <144709464+Ganapa260694@users.noreply.github.com>
2023-09-13 11:33:48 +0530

    day1 assaignment
    demo

---
+++ demo.java
@@ -0,0 +1,31 @@
+package week1.assaignment;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=25;
+int y=10;
+int z=x+y;
+int A=x-y;
+float f=15;
+double e=6.3;
+char c='s';
+boolean b=true;
+String name="LOGIC";
+System.out.println("add is:"+z);
+System.out.println("subtract is:"+A);
+System.out.println("multiply is:"+x*y);
+System.out.println("modulus is:"+x%y);
+System.out.println("divide is:"+x/y);
+System.out.println("float is:"+f);
+System.out.println("double is:"+e);
+System.out.println("char is:"+c);
+System.out.println("boolean is:"+b);
+System.out.println("string is:"+name);
+
+
+
+	}
+
+}
