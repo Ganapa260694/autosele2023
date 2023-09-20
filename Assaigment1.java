7c6c8ff52f0cb9aedc658be0dd68c49a3455e32f
Ganapa260694 <144709464+Ganapa260694@users.noreply.github.com>
2023-09-20 14:10:08 +0530

    week2.Assaigment1
    nested if else condition

--- Assaigment1.java
+++ Assaigment1.java
@@ -1,13 +1,38 @@
-package day2.week1;
+package week2.day1;
 
 public class Assaigment1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-int x=5;
-int y=10;
-int z=x*y;
-System.out.println(+z);
+		int income=500000;
+		if (income<0) {
+		System.out.println(income*0.00);	
+		}
+		else if(income<8925) {
+			System.out.println(income*0.10);	
+		}
+		else if(income<36250) {
+			System.out.println(income*0.15);	
+		}
+		else if(income<87850) {
+			System.out.println(income*0.23);	
+		}
+		else if(income<183250) {
+			System.out.println(income*0.28);	
+		}
+		else if(income<398350) {
+			System.out.println(income*0.33);	
+		}
+		else if(income<400000) {
+			System.out.println(income*0.35);	
+		}
+		else {
+			System.out.println(income*0.396);
+		}
+		
 	}
-
 }
+		
+	
+	
+
+
