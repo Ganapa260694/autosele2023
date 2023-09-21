f65d2769023841ae901972af1bc22f5dcb9d0350
Ganapa260694 <144709464+Ganapa260694@users.noreply.github.com>
2023-09-21 15:56:58 +0530

    Week2.day2
    Switch case

---
+++ LearnScase.java
@@ -0,0 +1,15 @@
+package week2.day2;
+
+public class LearnScase {
+
+	 public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String september="day2";
+      switch (september) {
+      case "day1": System.out.println("installation");break;
+      case "day2": System.out.println("conditional statement");break;
+      case "day3": System.out.println("reverse string");break;
+}
+	}
+
+}
