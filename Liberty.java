40b00fad13607be5a07b9ff80841141d561537e5
Ganapa260694 <144709464+Ganapa260694@users.noreply.github.com>
2023-09-22 15:51:24 +0530

    Week2.day3
    Method Overriding

---
+++ Liberty.java
@@ -0,0 +1,11 @@
+package week2.day3;
+
+public class Liberty {
+	
+	public void course1() {
+		System.out.println("Software testing course (JAVA)");
+	}
+	public void course2() {
+		System.out.println("Software testing course (PYTHON)");
+	}
+}
---
+++ Oceans.java
@@ -0,0 +1,15 @@
+package week2.day3;
+
+public class Oceans extends Liberty {
+  
+	public void course1() {
+		System.out.println("Choose LIBERTY for Software testing course (JAVA) ");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Liberty L=new Oceans ();
+       L.course1();
+       L.course2();
+	}
+
+}
